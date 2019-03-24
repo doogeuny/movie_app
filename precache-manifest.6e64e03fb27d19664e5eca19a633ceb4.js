7acbaff92ef1979bc1d4d9b3134eb981171ff3d4
daeKeun Kim <xeexxee2@gmail.com>
2019-03-24 20:51:52 +0900

    Updates

---
+++ precache-manifest.6e64e03fb27d19664e5eca19a633ceb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9e15cd8fd805b58e80e",
+    "url": "doogeuny.github.io/movieapp/static/js/runtime~main.fd796d2d.js"
+  },
+  {
+    "revision": "563f970258a56306d3c6",
+    "url": "doogeuny.github.io/movieapp/static/js/main.2cead16b.chunk.js"
+  },
+  {
+    "revision": "d29d4fc9f72638eb49f1",
+    "url": "doogeuny.github.io/movieapp/static/js/2.19428d17.chunk.js"
+  },
+  {
+    "revision": "563f970258a56306d3c6",
+    "url": "doogeuny.github.io/movieapp/static/css/main.00996c45.chunk.css"
+  },
+  {
+    "revision": "f201979bfd3fce37864a4eca1af317b1",
+    "url": "doogeuny.github.io/movieapp/index.html"
+  }
+];
